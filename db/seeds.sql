bba97acc1e7e0e5217c83b59d7a16dbf137f021c
Phantom <fay.ashbro@gmail.com>
2020-10-09 21:25:34 -0400

    buger seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Pizza Burger');
+INSERT INTO burgers (burger_name) VALUES ('Doughnut Burger');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bacon Cheese Burger', true);
