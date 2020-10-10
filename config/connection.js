b8fdb89ace5ba8e4573afcda5c2c0141673ca60d
Phantom <fay.ashbro@gmail.com>
2020-10-10 00:02:44 -0400

    heroku changes to connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,21 @@
 const mysql = require("mysql"); 
 
-const connection = mysql.createConnection({
-    host: "localhost", 
-    port: 3306, 
-    user: "root", 
-    password: "RamblingFun2020!", 
-    database: "burger_db"
-}); 
+const connection; 
 
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connetion = mysql.createConnection({
+    host: 'localhost', 
+    user: 'root', 
+    password: 'RamblingFun2020!',
+    database: 'burger_db'
   });
+}; 
+
+connection.connect();
 
   module.exports = connection;
+
+
+
